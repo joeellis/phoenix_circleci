9cddbdfd40def4b715b0ac97ee3ce92c29bbd1cd
Joe Ellis <joe@joeellis.la>
2017-08-17 15:37:34 -0500

    update with yarn lock

---
+++ assets/js/sample.test.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe("Sample Test", function() {
+  describe("this is a test", function() {
+    it("passes true", function() {
+      assert(true);
+    });
+  });
+});
